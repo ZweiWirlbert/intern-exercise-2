d7e92549901ff3620228d73954579cf396a7f36d
Vinh Thuy <rickhoang42@gmail.com>
2022-04-09 11:08:37 +0700

    feat: complete achievement part on mobile version

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
       boxShadow: {
         navbar: "3px -10px 20px rgba(0,0,0,0.2)",
         flag: "1px 1px 5px rgba(0,0,0,0.3)",
+        button: "2px 2px 5px rgba(0,0,0,0.6)",
+        achievement: "0px 12px 31px rgba(211,211,211,0.220744)",
       },
     },
   },
